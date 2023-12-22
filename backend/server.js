c11bfe592a2969555639738a819ffcc4f911fcc6
FaixyX <faizanfaizy97@gmail.com>
2023-12-22 14:38:12 +0500

    Vercel hosting

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
-// const cors = require('cors')
+Sconst cors = require('cors')
 const userRoutes = require('./routes/user')
 
 // express app
