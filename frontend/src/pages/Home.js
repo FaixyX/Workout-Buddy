636c18b52fb4efd29274403bf6ba41ea6756031d
FaixyX <faizanfaizy97@gmail.com>
2023-12-26 14:22:01 +0500

    vercel

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts', {
+      const response = await fetch('https://workout-buddy-tan.vercel.app/api/workouts', {
         headers: {
           'Authorization': `Bearer ${user.token}`
         }
