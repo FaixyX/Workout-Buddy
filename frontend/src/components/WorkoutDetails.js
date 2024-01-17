3981469a08b08154d3fb8ac6d831972e28831ea4
Faizan Faryad <faizanfaizy97@gmail.com>
2024-01-17 17:20:57 +0500

    Update WorkoutDetails.js

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -9,7 +9,7 @@ const WorkoutDetails = ({ workout }) => {
     if(!user){
       return
     }
-    const response = await fetch('https://workout-buddy-faixyxs-projects.vercel.app/api/workouts/' + workout._id, {
+    const response = await fetch('/api/workouts/' + workout._id, {
       method: 'DELETE',
       headers: {
         'Authorization': `Bearer ${user.token}`
